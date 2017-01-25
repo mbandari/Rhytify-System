19d0df5c63a707b943b2860d7b023c41bbcdb260
mbandari <mbandari@A7670F1D>
2017-01-25 11:31:08 +0530

    <Story ID : 1>(Added fetch operation)

--- ConsoleGIT/Program.cs
+++ ConsoleGIT/Program.cs
@@ -5,64 +5,91 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks; ;
+using System.Threading.Tasks; 
 
 namespace ConsoleGIT
 {
-    class Program
+   public class Program
     {
         public static string path;
         public static string fromDate = "01/01/2017";
-        public static string toDate = "21/01/2017";
+        public static string toDate = "25/01/2017";
 
       
         static DateTime fromdt = DateTime.ParseExact(fromDate, "dd/MM/yyyy", null);
         static DateTime todt = DateTime.ParseExact(toDate, "dd/MM/yyyy", null);
-        static string path = @"D:\GITSource\LibGit2Sharp5";
+        public static string RepoPath = @"D:\GITSource\LibGit2Sharp7";
+
        
+      
 
         public static void Main(string[] args)
         {
             Console.Clear();
             
+             //  var co = new CloneOptions();
+                CloneOptions co = new CloneOptions();
+                string un = "mbandari";
+                string pwd = "Jjong@310711";
+                Credentials ca = new UsernamePasswordCredentials() { Username = un, Password = pwd };
+                co.CredentialsProvider = (_url, _user, _cred) => ca;
+                co.IsBare = true;
+               if (Directory.Exists(RepoPath))
+              {
+                using (var repo = new Repository(@"D:\GITSource\LibGit2Sharp7"))
+                {
+                    foreach (Remote remote in repo.Network.Remotes)
+                    {
+                        FetchOptions options = new FetchOptions();
+                        options.CredentialsProvider = new CredentialsHandler(
+                                (url, usernameFromUrl, types) =>
+                    new UsernamePasswordCredentials()
+                    {
+                        Username = un,
+                        Password = pwd
+                    });
+                        repo.Network.Fetch(remote, options);
+                    }
+                }
+            }
+
+
             try
             {
-             var co = new CloneOptions();
-           
-            co.CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "mbandari", Password = "Jjong@310711" };
-            
-            co.IsBare = true;
-           
-            string  Mainpath =  Repository.Clone("https://github.com/photonstorm/phaser.git.", path, co);
+
+                //https://github.com/mbandari/Rhytify-System.git
+                //https://github.com/photonstorm/phaser.git
+                  string Mainpath =  Repository.Clone("https://github.com/mbandari/Rhytify-System.git", @"D:\GITSource\LibGit2Sharp7", co);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Fail GIT");               
+                Console.WriteLine("Fail Clone GIT");
+
+               
             }
-            using (var Git = new Repository(path))
+            using (var Git = new Repository(@"D:\GITSource\LibGit2Sharp7"))
             {
                 // No of commits
                 foreach (var Commit in Git.Commits)
                 {
-                    //foreach (var branch in Git.Branches)
-                    //{
-                    //    Console.WriteLine(branch.RemoteName);
-                    //}
-
                     // Changes in each commit
                     foreach (var parent in Commit.Parents)
                     {
+                        // Changes between from-date and to-date
                         if (Commit.Committer.When.DateTime > fromdt && Commit.Committer.When.DateTime < todt)
                         {
-                            Console.WriteLine("{0} | {1}", Commit.Sha, Commit.MessageShort);
+                            //Console.WriteLine("{0} | {1}", Commit.Sha, Commit.MessageShort);
+                            
+                            // Change fileNames
                             foreach (TreeEntryChanges change in Git.Diff.Compare<TreeChanges>(parent.Tree, Commit.Tree))
                             {
                                 Console.WriteLine("{0} : {1}", change.Status, change.Path);
+                                Console.WriteLine("{0} by {1}",
+                                   Commit.Committer.When.DateTime,
+                                   Commit.Id.ToString(7),
+                                   Commit.Author.Name);
                             }
-                            Console.WriteLine("{0} by {1}",
-                                    Commit.Committer.When.DateTime,
-                                    Commit.Id.ToString(7),
-                                    Commit.Author.Name);
+                           
 
                         }
                         
--- GITRhytify/Controllers/HomeController.cs
+++ GITRhytify/Controllers/HomeController.cs
@@ -10,12 +10,37 @@ namespace GITRhytify.Controllers
 {
     public class HomeController : Controller
     {
-       // Comments added ;
+        // Comments added ;
+        public string serverAddress = "";
+        public string userName = "";
+        public string passWord = "";
+        //private static System.Collections.ObjectModel.ReadOnlyCollection<CatalogNode> staticTpcNodes;
 
+        public ActionResult GIT()
+        {
+            ViewBag.Message = "Your GIT page.";
+
+            return View();
+        }
 
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string serveraddress, string username, string password)
+        {
+            if (serveraddress == "" || username == "" || password == "")
+            {
+                ViewBag.Message = "Login Failed. Please Enter the Details Correctly";
+                ViewBag.HasMessage = "true";
+                return View("Index");
+            }
+            else
+            {
+                return RedirectToAction("ControlTree", "Home", new { serveraddress = serveraddress, username = username, pwd = password });
+            }
+          //  return View();
         }
 
         public ActionResult About()
@@ -30,6 +55,34 @@ namespace GITRhytify.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ControlTree(string serveraddress, string username, string password)
+        {
+
+            // serverAddress = ConfigurationManager.AppSettings["URL"].ToString();  
+            // userName = ConfigurationManager.AppSettings["UserName"].ToString();
+            // passWord = ConfigurationManager.AppSettings["PassWord"].ToString();
+            serverAddress = serveraddress;
+            userName = username;
+            passWord = password;
+            var ProjectCollectionList = new List<string>();
+            var test = new Dictionary<string, string>();
+            if (serverAddress != "")
+            {
+                //staticTpcNodes = objTFSDeltaClass.TFSConnectionDetails(serverAddress, userName, passWord);
+                //staticTpcNodes.Take(staticTpcNodes.Count - 1);
+            }
+            //if (staticTpcNodes != null)
+            //{
+            //    foreach (CatalogNode tpcNode in staticTpcNodes)
+            //    {
+            //        teamCollectionList.Add(tpcNode.Resource.DisplayName);
+            //    }
+            //}
+
+
+            return View("Control Tree");
         }
     }
 }
