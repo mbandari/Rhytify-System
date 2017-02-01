157c07c0cb0c3c6cd58596848f7136dbe25e0c3f
mbandari <mbandari@A7670F1D>
2017-02-01 19:04:56 +0530

    <UserStory=1, TestCase=2, Task=3>(ReturnTypeCheck Added)

---
+++ ConsoleGIT/ReturnTypeCheck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGIT
+{
+   public class ReturnTypeCheck
+    {
+    
+        public void ShowInt()
+        {
+            Console.WriteLine("Void");
+        }
+
+        public void Main()
+        {
+            ShowInt();
+        }
+    }
+
+    
+}
